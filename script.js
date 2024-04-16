3abbeb485c26ac31c6f797603da22e96bab8939d
AbuTalha3 <najeebakhun@gmail.com>
2024-04-16 13:16:50 +0500

    Add step 9

---
+++ script.js
@@ -0,0 +1,17 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+
+  })
+}
+
+sortButton.addEventListener("click", sortInputArray);
