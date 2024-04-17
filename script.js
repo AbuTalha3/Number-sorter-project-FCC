14c2278afe3364bc995a6c637c7166d1bfaf9088
AbuTalha3 <najeebakhun@gmail.com>
2024-04-17 05:42:33 +0500

    Add step 16 for loop

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+
+
+
+  updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
-
+    outputValueNode.innerText = num;
   })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
+    }
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
