989b1a306d668de044f0017a2f4e7b9e2c19a533
AbuTalha3 <najeebakhun@gmail.com>
2024-04-17 05:56:36 +0500

    step 19

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-const sortedValues = bubbleSort(inputValues);
+  const sortedValues = bubbleSort(inputValues);
 
   updateUI(sortedValues);
 }
@@ -23,6 +23,12 @@ const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
       console.log(array, array[j], array[j + 1]);
+
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j +1];
+        array[j +1] = temp;
+      }
     }
   }
 }
